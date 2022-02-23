a36bfe0c3f05c4491efcd3b27ff7e740fa601906
svetlana <svetakam2000@gmail.com>
2022-02-23 20:29:44 +0500

    new file brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+"""Finding the greatest common divisor."""
+
+
+from brain_games.games.gcd import description, game_with_answers
+from brain_games.main_logic import main_logic
+
+
+def main():
+    """Brain gcd game."""
+    main_logic(game_with_answers, description)
+
+
+if __name__ == '__main__':
+    main()
